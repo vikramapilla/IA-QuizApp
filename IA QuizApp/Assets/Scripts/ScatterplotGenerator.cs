b767254af9f9a8a2dfa8e834b29b22797fbf03ac
kartikprabhu20 <kartikprabhu20@yahoo.co.in>
2020-03-31 21:39:20 +0200

    color reverted

--- IA QuizApp/Assets/Scripts/ScatterplotGenerator.cs
+++ IA QuizApp/Assets/Scripts/ScatterplotGenerator.cs
@@ -15,10 +15,10 @@ public class ScatterplotGenerator : MonoBehaviour
     DataReader reader;
 
 
-    Color32[] colors = { new Color32(229, 115, 115, 1), new Color32(255,138,101 ,1), new Color32(0,172,193 ,1),
-    new Color32(100, 181, 246, 1)};
-
     Color highlightGlyphColor = Color.red;
+
+    Color32[] colors = {  new Color32(255, 132, 0, 255), new Color32(66, 245, 66, 255), new Color32(250, 250, 0, 255),
+                         new Color32(200, 0, 250, 255),new Color32(250, 0, 0, 255),new Color32(66, 66, 245, 255) }; //{Orange,Green, Yellow, Red, Purple,Blue
 
     DataReader.DataPoint[] dataPoints;
 
@@ -236,7 +236,7 @@ public class ScatterplotGenerator : MonoBehaviour
                     break;
 
                 default: 
-                    glyphList[index].GetComponent<Renderer>().material.color = colors[0];
+                    glyphList[index].GetComponent<Renderer>().material.color = colors[5];
                     break;
             }
             index++;
